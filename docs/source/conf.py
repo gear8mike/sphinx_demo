d90985b842a9eab82bfe49e79fd792cbe786892b
Misha Smirnov <msmirnov@km3net.de>
2023-02-21 02:40:12 +0100

    some changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,3 +81,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Pathes for sphinx gallery
+sphinx_gallery_conf = {
+    "examples_dirs": ["../../examples"],  # path to your example scripts
+    "gallery_dirs": ["auto_examples"],  # path to where to save gallery generated output
+}
